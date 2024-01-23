89b4e9d49a3ed77669995fcc5b96b9a9f4cd8659
PoloBaca <thebigbaca@gmail.com>
2024-01-22 22:06:58 -0600

    Commit Java Book 2

---
+++ HelloWorld/src/dinosaur.java
@@ -0,0 +1,9 @@
+public class dinosaur {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mesozoic Eden");
+        String position = "Park Manager";
+        String name = "Jorge Vargas";
+
+        System.out.println("My name is " + name + "and I want to be a " + position + " in Mesozoic Eden.");
+    }
+}
